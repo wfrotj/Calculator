9248fa45b2684d6ec70167314c49f13fed9a23e7
wfortj <trebrodrigo@gmail.com>
2023-04-15 17:27:45 +0800

    add initial JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+const calculator = (() => {
+  const add = (a, b) => a + b;
+  const minus = (a, b) => a - b;
+  const times = (a, b) => a * b;
+  const divide = (a, b) => a / b;
+
+  return { add, minus, times, divide };
+})();
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    let head = document.getElementById("head");
+    head.textContent = button.innerText;
+  });
+});

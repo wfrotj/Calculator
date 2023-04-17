9287c1fcdf59dc219498de17f428adae45a0711d
wfortj <trebrodrigo@gmail.com>
2023-04-18 00:56:25 +0800

    change layout

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const calculator = (() => {
+/*const calculator = (() => {
   const add = (a, b) => a + b;
   const minus = (a, b) => a - b;
   const times = (a, b) => a * b;
@@ -15,3 +15,34 @@ buttons.forEach(function (button) {
     head.textContent = button.innerText;
   });
 });
+*/
+
+let currentInput = "";
+let previousInput = "";
+const numberButtons = document.querySelectorAll(".is-num");
+const largeDisplay = document.querySelector(".large-display");
+const operatorsButton = document.querySelectorAll(".operators");
+const clearButton = document.querySelector(".clear");
+const smallDisplay = document.querySelector(".small-display");
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    currentInput += event.target.textContent;
+    largeDisplay.textContent = currentInput;
+  });
+});
+
+clearButton.addEventListener("click", () => {
+  currentInput = "";
+  largeDisplay.textContent = currentInput;
+});
+
+operatorsButton.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    smallDisplay.textContent = largeDisplay.textContent;
+  });
+});
+
+function clearInputs() {
+  currentInput = "";
+}
